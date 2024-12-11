7cd18acc9d34c39bb5ea4837a142d8922f3dde82
Iftekhar Md. Shishir <iftekharweb@gmail.com>
2024-12-11 15:26:00 +0600

    admin pannel has been customized

--- backend/core/admin.py
+++ backend/core/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from .models import User
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+    list_display = ['id', 'email', 'name', 'is_admin', 'is_active']
+    list_filter = ['is_admin', 'is_active']
+    ordering = ['id']
 
-# Register your models here.
+    fieldsets = [
+        ['User Info', {'fields': ['email', 'name', 'password']}],
+        ['Permissions', {'fields': ['is_admin', 'is_active']}],
+    ]
+
+    add_fieldsets = [
+        [
+            None,
+            {
+                'classes': ['wide'],
+                'fields': ['email', 'name', 'password1', 'password2'],
+            },
+        ]
+    ]
+
+    search_fields = ['email', 'name']
+    filter_horizontal = []  
--- backend/notes/admin.py
+++ backend/notes/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import Topic, Note
 
-# Register your models here.
+@admin.register(Topic)
+class TopicAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'user']
+    search_fields = ['title', 'user__username']
+    list_filter = ['user',]
+    ordering = ['id',]
+    autocomplete_fields = ['user',] 
+
+@admin.register(Note)
+class NoteAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'topic', 'is_favourite']
+    search_fields = ['title', 'topic__title']
+    list_filter = ['is_favourite', 'topic']
+    ordering = ['id',]
+    autocomplete_fields = ['topic',]
